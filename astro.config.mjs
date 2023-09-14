506dcc3b70a967fe2c8230ac10b49a6ec2785948
PnrmShinyaMaeda <shinya.maeda@pnrm.co.jp>
2023-09-14 12:13:10 +0900

    consoleの削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import glsl from 'vite-plugin-glsl'
 
+const isDev = process.env.NODE_ENV !== "production";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shinyam61.github.io',
@@ -34,6 +36,9 @@ export default defineConfig({
         }
       },
       minify: true
+    },
+    esbuild: {
+      drop: isDev ? [] : ["console", "debugger"],
     },
     plugins: [
       ((obj) => {
